1fd773136360a7e1bfcd72ba24a38f487713e08f
Andrey Kotsar <andrey.kotsar@clearpoint.co.nz>
2016-07-24 16:50:36 +1200

    Configured automatic download of the IE driver

--- config/protractor.conf.ie.js
+++ config/protractor.conf.ie.js
@@ -3,7 +3,7 @@ var helpers = require('./helpers');
 var config = require('./protractor.conf.js').config;
 
 /* Uncomment to use the already running instance of the IE Driver Server */
-config.seleniumAddress = 'http://localhost:5555/';
+// config.seleniumAddress = 'http://localhost:5555/';
 
 config.directConnect = false; // Required for IE
 
@@ -12,6 +12,6 @@ config.capabilities = {
 };
 
 /* Uncomment to start the IE driver from the test */
-//config.seleniumArgs = ['-Dwebdriver.ie.driver=D:\\Temp\\IE\\32\\IEDriverServer.exe'];
+config.seleniumArgs = ['-Dwebdriver.ie.driver=node_modules/protractor/node_modules/webdriver-manager/selenium/IEDriverServer_Win32_2.53.1.exe'];
 
 exports.config = config;
